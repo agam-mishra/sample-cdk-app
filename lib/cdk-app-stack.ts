06e7cfd05ca779d4e03ac4d57b1e0dc5ed7276d0
agam-mishra <agammishra056@gmail.com>
2022-04-29 07:03:22 +0530

    Sample CDK app

---
+++ lib/cdk-app-stack.ts
@@ -0,0 +1,26 @@
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as cdk from 'aws-cdk-lib';
+
+export class CdkAppStack extends cdk.Stack {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // 👇 use the Bucket construct
+    const bucket = new s3.Bucket(this, 'avatars-bucket', {
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
+    // 👇 use the Table construct
+    const table = new dynamodb.Table(this, 'todos-table', {
+      partitionKey: {name: 'todoId', type: dynamodb.AttributeType.NUMBER},
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
+    new cdk.CfnOutput(this, 'bucketName', {
+      value: bucket.bucketName,
+    });
+    new cdk.CfnOutput(this, 'tableName', {value: table.tableName});
+  }
+}
